7ee329574c753b4aad190a56d56e6a42e3bb6a34
Jonah Duckles <jonah@duckles.org>
2015-03-01 14:00:03 -0600

    shebang

--- installers/debian.sh
+++ installers/debian.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 UPDATE_URL="https://raw.github.com/jduckles/minecraft-server-manager/master"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/msmcommon.sh
 source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
